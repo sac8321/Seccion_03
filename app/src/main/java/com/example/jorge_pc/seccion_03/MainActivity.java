c256d5ede5641cb80c08326ce3f56f33ef98d335
JORGE-PC <sac8321@gmail.com>
2018-01-06 16:33:42 -0600

    Seccion 57 parte 3

--- app/src/main/java/com/example/jorge_pc/seccion_03/MainActivity.java
+++ app/src/main/java/com/example/jorge_pc/seccion_03/MainActivity.java
@@ -2,12 +2,52 @@ package com.example.jorge_pc.seccion_03;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private List<String> names;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        names=this.getAllNames();
+        mRecyclerView=(RecyclerView) findViewById(R.id.recycler_view);
+        mLayoutManager=new LinearLayoutManager(this);
+
+        mAdapter= new MyAdapter(names, R.layout.reycler_view_item, new MyAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(String name, int position) {
+                Toast.makeText(MainActivity.this,name+"-"+position,Toast.LENGTH_LONG).show();
+            }
+        });
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+
+    }
+
+
+    private List<String> getAllNames(){
+        return new ArrayList<String>(){{
+            add("Juan");
+            add("Jorge");
+            add("Jose");
+            add("Magaly");
+            add("Monica");
+            add("Andrea");
+            add("Julio");
+
+        }};
     }
 }
