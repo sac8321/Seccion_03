ad6619f5ba697d7b1668d0c97502c4d5f212d21e
JORGE-PC <sac8321@gmail.com>
2018-01-07 14:42:32 -0600

    Seccion 57
    Recycler view mejorado
    Agrega y elimina listview

--- app/src/main/java/com/example/jorge_pc/seccion_03/MainActivity.java
+++ app/src/main/java/com/example/jorge_pc/seccion_03/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.jorge_pc.seccion_03;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -15,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    private int counter=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,15 +32,53 @@ public class MainActivity extends AppCompatActivity {
         mAdapter= new MyAdapter(names, R.layout.reycler_view_item, new MyAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(String name, int position) {
-                Toast.makeText(MainActivity.this,name+"-"+position,Toast.LENGTH_LONG).show();
+                //Toast.makeText(MainActivity.this,name+"-"+position,Toast.LENGTH_LONG).show();
+                deleteName(position);
             }
         });
 
+        //performance
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setAdapter(mAdapter);
 
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+     getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.add_name:
+                this.addName(0);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+
+
+    }
+
+    private void addName(int position) {
+        names.add(position,"New name"+(++counter));
+        mAdapter.notifyItemInserted(position);
+        mLayoutManager.scrollToPosition(position);
+    }
+
+    private void deleteName(int position){
+        names.remove(position);
+        mAdapter.notifyItemRemoved(position);
+
+
+    }
 
     private List<String> getAllNames(){
         return new ArrayList<String>(){{
